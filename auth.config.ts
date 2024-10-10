e3acf415ff4b12d30142474a8682310d76708534
1998SUIYUE <593180497@qq.com>
2024-10-11 06:33:38 +0800

    222

--- auth.config.ts
+++ auth.config.ts
@@ -17,20 +17,24 @@ export const authConfig = {
       
       console.log("--------是否登录--------", isLoggedIn);
       console.log("--------是否在仪表板页面--------", isOnDashboard);
+      console.log("--------当前路径--------", nextUrl.pathname);
+
+      if (isLoggedIn) {
+        if (!isOnDashboard) {
+          console.log("--------用户已登录但不在仪表板页面，重定向到仪表板--------");
+          const dashboardUrl = new URL('/dashboard', nextUrl.origin);
+          console.log("----------------重定向到----------", dashboardUrl.toString());
+          return Response.redirect(dashboardUrl);
+        }
+        console.log("--------用户已登录且在仪表板页面，允许访问--------");
+        return true;
+      }
 
       if (isOnDashboard) {
-        if (isLoggedIn) {
-          console.log("--------用户已登录且在仪表板页面，允许访问--------");
-          return true;
-        }
         console.log("--------用户未登录但尝试访问仪表板，拒绝访问--------");
         return false; // Redirect unauthenticated users to login page
-      } else if (isLoggedIn) {
-        console.log("--------用户已登录但不在仪表板页面，重定向到仪表板--------");
-        const dashboardUrl = new URL('/dashboard', nextUrl.href);
-        console.log("----------------重定向到----------", dashboardUrl.toString());
-        return Response.redirect(dashboardUrl);
       }
+
       console.log("--------允许访问非仪表板页面--------");
       return true;
     },
