12ab6955c6c7df29cf1716a1d069588600a6ce70
1998SUIYUE <593180497@qq.com>
2024-10-11 06:13:53 +0800

    3

--- auth.config.ts
+++ auth.config.ts
@@ -11,13 +11,14 @@ export const authConfig = {
   ],
   callbacks: {
     authorized({ auth, request: { nextUrl } }) {
-      console.log("auth nexturl",auth,nextUrl)
+      console.log("---------auth nexturl---------",auth,nextUrl)
       const isLoggedIn = !!auth?.user;
       const isOnDashboard = nextUrl.pathname.startsWith('/dashboard');
       if (isOnDashboard) {
         if (isLoggedIn) return true;
         return false; // Redirect unauthenticated users to login page
       } else if (isLoggedIn) {
+        console.log("----------------重定向到----------",new URL('/dashboard', nextUrl))
         return Response.redirect(new URL('/dashboard', nextUrl));
       }
       return true;
